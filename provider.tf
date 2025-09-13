80ad01f1f41088e35e76b2e96a146530179d5270
JIBAN KUMAR SWAIN <54506743+JibanKumarSwain@users.noreply.github.com>
2025-09-13 12:51:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,15 @@ terraform {
     }
   }
 }
+  backend "s3" {                                         # for that we create a separest folder the name is remote-setup we do the setup of terraform backend
+  bucket = "terraform-s3-infrasetup"
+  key = "terraform.tfstate"
+  region = "ap-south-1"
+  dynamodb_table = "remote-backend"
+  }
+}
 
 provider "aws" {
   region = "ap-south-1"
+
 }
